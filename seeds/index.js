b34cec09039c12d3e9544a40bfdbcdaffe1d2115
charr1309 <ceh710@aol.com>
2021-06-24 12:54:50 -0400

    added seed data

---
+++ seeds/index.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+//empty database
+const seedDB = async () => {
+    await Campground.deletMany({});
+    const c = new Campground({ title: 'purple field' });
+    await c.save();
+}
+
+seedDB();
