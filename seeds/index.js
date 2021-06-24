584c2149a9de11a546011910213794f54ff6d29f
charr1309 <ceh710@aol.com>
2021-06-24 15:23:53 -0400

    updates to seeds/index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
+const cities = require('./cities');
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -14,11 +15,16 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
-//empty database
+
 const seedDB = async () => {
-    await Campground.deletMany({});
-    const c = new Campground({ title: 'purple field' });
-    await c.save();
+    await Campground.deleteMany({});    
+    for(let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+           location: `${cities[random1000].city}, ${cities[random1000].state}` 
+        })
+        await camp.save();
+    }
 }
 
 seedDB();
