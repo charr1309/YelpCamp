e55ecbf84eca4c12ed3c4890bd2fc6a6a07034d7
charr1309 <ceh710@aol.com>
2021-07-18 18:09:28 -0400

    error implementing lecture 484

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const Campground = require('./models/campground');
-const methodOverride = require('method-override');//allows us to alter the method for our form
-const catchAsync = require('./utils/catchAsync');
-const { campgroundSchema, reviewSchema } = require('./schemas.js');
+// const Campground = require('./models/campground');
 const ExpressError = require('./utils/ExpressError');
-const Review = require ('./models/review');
+const methodOverride = require('method-override');//allows us to alter the method for our form--still need method="POST" after _method
+// const catchAsync = require('./utils/catchAsync');
+// const { campgroundSchema, reviewSchema } = require('./schemas.js');
+// const Review = require ('./models/review');
+
+
+const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');//require contents of the reviews file in the folder routes which is the route
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -24,11 +30,12 @@ db.once("open", () => {
 const app = express();
 
 app.engine('ejs', ejsMate);//template engine to use static template files to design HTML easier
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');//--ejs(embedded Javascript) is a templating language/engine that lets its user generate HTML with plain javascript
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))//must tell express to parse the body so use this or nothing will come back when you use req.body for anything
 app.use(methodOverride('_method'));//tell express to use methodOverride
+app.use(express.static(path.join(__dirname, 'public')))//added to tell express, with static assests to use the path to the public directory
 
 const validateCampground = (req,res,next) => {    
     const {error} = campgroundSchema.validate(req.body);
@@ -41,85 +48,27 @@ const validateCampground = (req,res,next) => {
     
 }
 
-const validateReview = (req,res,next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
+// const validateReview = (req,res,next) => {
+//     const {error} = reviewSchema.validate(req.body);
+//     if (error) {
+//         const msg = error.details.map(el => el.message).join(',')
+//         throw new ExpressError(msg, 400)
+//     } else {
+//         next();
+//     }
+// }
+
+app.use('/campgrounds', campgrounds);//tells express that any route that starts with /campgrounds, to use the campgrounds routes in campgrounds.js--since this will start all of the routes with /campgrounds here, the /campgrounds will be removed from the routes in campgrounds.js file
+app.use('/campgrounds/:id/reviews', reviews);//to have access to the :id part of this route, merge params must be in the parenthesis of the router statement at the top of the reviews.js file like so:   const router = express.Router({mergeParams: true});
+
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({});//finds all campgrounds
-    res.render('campgrounds/index.ejs', { campgrounds })//pass campgrounds to template
-})
 
-//this route below has to come before the :id route. If it doesnt the brower will match the :id route and since there is no new, id, you will get a time out error, so order does matter
 
-app.get('/campgrounds/new', (req,res) =>{//dont need async function here since its a form and wont be getting or posting until form is submitted
-    res.render('campgrounds/new');
-})
 
-//req.body had to be parsed above. We add app.use(express.urlencode)...
-//code below sets up the endpoint from the form in campgrounds/new as a post request
-
-app.post('/campgrounds', validateCampground, catchAsync(async(req,res,next) => {
-    //if(!req.body.campground) throw new ExpressError('Invalid campground data', 400);-- this line just checks to see if the req.body has campground--adding Joi for validation will allow much more detail validation for all values--campground is the key and all the values are under campground (ex. campground[price] or campground[description] etc.)--moved the joi schema from below to a function about so it can just be called on the other routes
-
-    
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.get('/campgrounds/:id', catchAsync(async (req,res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
-    console.log(campground);
-    res.render('campgrounds/show', { campground })
-}))
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit', { campground })
-}))
-
-app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params;
-
-    //using the spread operator ...req.body.campground spreads that object into the object found from the id searched for in Campground.findByIdAndUpdate 
-
-    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground})
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete('/campgrounds/:id', catchAsync(async (req ,res) => {
-    const {id} = req.params;
-    await Campground.findByIdAndDelete(id);//findByIdAndDelete() triggers only the delete middleware, findOneAndDelete added to in campgrounds.js--if another delete or remove method is used, it will not trigger
-    res.redirect('/campgrounds');
-}))
-
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);//in the form show page each input was givin a name where data was stored under review ie. name="review[rating]" so its all under the key of review once its been parsed
-    campground.reviews.push(review);//in campground model the property on the CampgroundSchema was set to reviews plural which is an array of id's that correspond to a review and the review id is pushed there
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground.id}`);//redirect to the show page
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    //--the $pull operator removes from an existing array all instances of a value or values that match a specified condition--reviews is just an array of id's
-    const {id, reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})//removes reference from the array
-    await Review.findByIdAndDelete(req.params.reviewId);//delete entire review--function triggers the middleware, findOneAndDelete()
-    res.redirect(`/campgrounds/${id}`);
-}))
 
 
 
---
+++ routes/reviews.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router({mergeParams: true});//express router likes to keep params seperate so mergeParams: true is required to access reviews for each campground since the reviews routes are in a different file in the routes folder
+
+const Campground = require('../models/campground');
+const Review = require ('../models/review');
+
+const { reviewSchema } = require('../schemas.js');
+
+const ExpressError = require('../utils/ExpressError');
+const catchAsync = require('../utils/catchAsync');//need 2 dots since this file is nested in the routes folder
+
+const validateReview = (req,res,next) => {
+    const {error} = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+
+router.post('/', validateReview, catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);//in the form show page each input was givin a name where data was stored under review ie. name="review[rating]" so its all under the key of review once its been parsed
+    campground.reviews.push(review);//in campground model the property on the CampgroundSchema was set to reviews plural which is an array of id's that correspond to a review and the review id is pushed there
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground.id}`);//redirect to the show page
+}))
+
+router.delete('/:reviewId', catchAsync(async (req, res) => {
+    //--the $pull operator removes from an existing array all instances of a value or values that match a specified condition--reviews is just an array of id's
+    const {id, reviewId} = req.params;
+    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})//removes reference from the array
+    await Review.findByIdAndDelete(req.params.reviewId);//delete entire review--function triggers the middleware, findOneAndDelete()
+    res.redirect(`/campgrounds/${id}`);
+}))
+
+module.exports = router;
