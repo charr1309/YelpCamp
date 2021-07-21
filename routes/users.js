a689f8d4c4bb2cfbb71aeb2c4df4fc0456352a7c
charr1309 <ceh710@aol.com>
2021-07-21 05:09:13 -0400

    fixed 508 lecture error--had 2 post routes in users.js

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,9 @@ const User = require('../models/user');
 router.get('/register', (req, res) => {//this route will just render a form/template to create a new user
     res.render('users/register');
 })
-router.post('/register', async (req, res) => {//this is the route that we submit the new user too and that is a post route to slash register from the form in register.ejs
-    res.send(req.body)
-});
+// router.post('/register', async (req, res) => {//this is the route that we submit the new user too and that is a post route to slash register from the form in register.ejs
+//     res.send(req.body)
+// });
 
 //now that the form is created, use the form data to create a new user
 
