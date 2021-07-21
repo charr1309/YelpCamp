430563ff0d7610a7dbfcf455ae59837455c0db23
charr1309 <ceh710@aol.com>
2021-07-21 07:32:50 -0400

    added auto login for new users

--- routes/users.js
+++ routes/users.js
@@ -13,14 +13,20 @@ router.get('/register', (req, res) => {//this route will just render a form/temp
 
 //now that the form is created, use the form data to create a new user
 
-router.post('/register', catchAsync(async (req, res) => {//wrap function in catchAsync to catch any errors from mongoose
+router.post('/register', catchAsync(async (req, res, next) => {//wrap function in catchAsync to catch any errors from mongoose--add next to account for the required callback function in the req.login below
     try {//added try catch to handle error on the same page as the form so you dont go to an error page when the catchAsync finds the error
     const { email, username, password } = req.body;//destructure what is needed from req.body
     const user = new User({email, username});//make a user model instance(models/user.js) where the username and email is passed in an object but not the password then call user.register
     const registeredUser = await User.register(user, password);//User.register takes in the new user just created above and takes the password and hash it and add salt and adds it to the new user--saves that in registerdUser
-    console.log(registeredUser);
-    req.flash('success','Welcome to Yelp Camp');
-    res.redirect('/campgrounds');
+    // console.log(registeredUser);
+
+    //req.login comes automatically with passport like req.logout and allows the new user to be automatically logged in when they register and not have to go to the login page--the req.login function requires a callback so you cannot await it and the callback is an error callback
+    req.login(registeredUser, err => {
+        if (err) return next(err);
+        req.flash('success','Welcome to Yelp Camp');
+        res.redirect('/campgrounds');
+    })
+    
     } catch(e){
         req.flash('error', e.message);
         res.redirect('register');
