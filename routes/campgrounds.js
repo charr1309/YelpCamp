bd6ccd3b395a8982fcda8a5166be543e50b79b8e
charr1309 <ceh710@aol.com>
2021-07-21 04:50:55 -0400

    added isLoggedIn middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,7 @@ const catchAsync = require('../utils/catchAsync');//need 2 dots since this file 
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const { campgroundSchema } = require('../schemas.js');
+const { isLoggedIn } = require('../middleware');
 
 const validateCampground = (req,res,next) => {    
     const {error} = campgroundSchema.validate(req.body);
@@ -24,14 +25,16 @@ router.get('/', async (req, res) => {
 
 //this route below has to come before the :id route. If it doesnt the brower will match the :id route and since there is no new, id, you will get a time out error, so order does matter
 
-router.get('/new', (req,res) =>{//dont need async function here since its a form and wont be getting or posting until form is submitted
+router.get('/new', isLoggedIn, (req,res) =>{//dont need async function here since its a form and wont be getting or posting until form is submitted    
     res.render('campgrounds/new');
 })
 
 //req.body had to be parsed above. We add router.use(express.urlencode)...
 //code below sets up the endpoint from the form in campgrounds/new as a post request
 
-router.post('/', validateCampground, catchAsync(async(req,res,next) => {
+//adding isLoggedIn to this post route protects the route from a user using something like postman to access the route--the user would not see this route if they are not logged in to the site itself but the route is not protected from outside requests by other programs unless isLoggedIn is included on the route
+
+router.post('/', isLoggedIn, validateCampground, catchAsync(async(req,res,next) => {
     //if(!req.body.campground) throw new ExpressError('Invalid campground data', 400);-- this line just checks to see if the req.body has campground--adding Joi for validation will allow much more detail validation for all values--campground is the key and all the values are under campground (ex. campground[price] or campground[description] etc.)--moved the joi schema from below to a function about so it can just be called on the other routes
         
     const campground = new Campground(req.body.campground);
@@ -50,7 +53,7 @@ router.get('/:id', catchAsync(async (req,res) => {
     res.render('campgrounds/show', { campground })
 }))
 
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     if(!campground) {
         req.flash('error', 'Cannot find that campground!');
@@ -59,7 +62,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground })
 }))
 
-router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
 
     //using the spread operator ...req.body.campground spreads that object into the object found from the id searched for in Campground.findByIdAndUpdate 
@@ -69,7 +72,7 @@ router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.delete('/:id', catchAsync(async (req ,res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req ,res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);//findByIdAndDelete() triggers only the delete middleware, findOneAndDelete added to in campgrounds.js--if another delete or remove method is used, it will not trigger
     req.flash('success', 'Successfully deleted campground!')
