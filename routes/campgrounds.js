bb71eb23541af526a3eda0395e65aaf24223cb0e
charr1309 <ceh710@aol.com>
2021-07-25 04:42:16 -0400

    restructure routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -8,25 +8,27 @@ const { isLoggedIn, isAuthor, validateCampground, validateReview } = require('..
 
 //moved campground routes to this campgrounds.js file in the routes folder so /campgrounds is removed now that they are connected in the index.js file via app.use('campgrounds', campgrounds)
 
-router.get('/', isLoggedIn, catchAsync(campgrounds.index));
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
-//this route below has to come before the :id route. If it doesnt the brower will match the :id route and since there is no new, id, you will get a time out error, so order does matter
+    //this route below has to come before the :id route. If it doesnt the brower will match the :id route and since there is no new, id, you will get a time out error, so order does matter
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
+
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
+
+
+
 
 //req.body had to be parsed above. We add router.use(express.urlencode)...
 //code below sets up the endpoint from the form in campgrounds/new as a post request
 
 //adding isLoggedIn to this post route protects the route from a user using something like postman to access the route--the user would not see this route if they are not logged in to the site itself but the route is not protected from outside requests by other programs unless isLoggedIn is included on the route
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-
-router.get('/:id', catchAsync(campgrounds.showCampground))
-
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,21 +5,21 @@ const catchAsync = require('../utils/catchAsync');//require to catch any mongoos
 const User = require('../models/user');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegister)
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register))
+
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
+
+router.get('/logout', users.logout)
+
 // router.post('/register', async (req, res) => {//this is the route that we submit the new user too and that is a post route to slash register from the form in register.ejs
 //     res.send(req.body)
 // });
 
 //now that the form is created, use the form data to create a new user
 
-router.post('/register', catchAsync(users.register));
-
 //2 login routes, one will serve a form and the other will be a post for /login as well which will do the actual logging in when it makes sure your credentials are valid
-
-router.get('/login', users.renderLogin)
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
-
-router.get('/logout', users.logout)
-
 module.exports = router;
