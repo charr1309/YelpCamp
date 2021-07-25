f40aceafe4a85bedf91a0b70e6c111d32f1ab31d
charr1309 <ceh710@aol.com>
2021-07-25 18:13:26 -0400

    lecture 527 multer middleware--not working

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,13 +6,21 @@ const Campground = require('../models/campground');
 
 const { isLoggedIn, isAuthor, validateCampground, validateReview } = require('../middleware');
 const multer = require ('multer');
-const upload = multer({ dest: 'uploads/' });//initialize or execute multer by passing in a configuration object
+const { storage } = require('../cloudinary');//changes the upload storage location to cloudinary that was just set up in the cloudinary folder--lecture 530, instead--node automatically looks for an index file in a folder so '../cloudinary/index is not needed in the require
+
+//const upload = multer({ dest: 'uploads/' });--was the set up code when multer was installed to initialize or execute multer by passing in a configuration object {dest: 'uploads/} which is telling to store uploads locally in the uploads folder
+
+const upload = multer({ storage });//refactor multer to use storage instead of dest: 'uploads/'
 
 //moved campground routes to this campgrounds.js file in the routes folder so /campgrounds is removed now that they are connected in the index.js file via app.use('campgrounds', campgrounds)
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    //.post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req, res) =>{//this is uploading from multer with upload.single or upload.array for example, and told multer to store things in a different destination above,--lecture 527  
+        console.log(req.body, req.files);//use req.file for single upload and upload.array uses req.files
+        res.send('IT WORKED?!')
+    })
 
     //this route below has to come before the :id route. If it doesnt the brower will match the :id route and since there is no new, id, you will get a time out error, so order does matter
 
